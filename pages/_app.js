26a382ea0f0a44e6e419976303e19e77e79186eb
richardg91-bien <richardg91@gmail.com>
2021-03-18 19:45:41 -0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import 'bootswatch/dist/cosmo/bootstrap.min.css'
+
+import '../global.css'
+
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
+
+
+
+
+  export default MyApp
---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>My Portfolio</title>
+          <meta name= "description" content="Richard García Portfolio Website"></meta>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
